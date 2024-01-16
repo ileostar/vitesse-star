194376fe3e82a283a2579a8e6e86d22d4abf0cb9
ileostar <hi@leostar.top>
2024-01-16 18:36:36 +0800

    :recycle: remove no must code

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,19 @@ import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import OnuUI from 'onu-ui'
 import App from './App.vue'
 
+import store from './store'
 import '@unocss/reset/tailwind.css'
 import './styles/main.css'
 import 'onu-ui/dist/style.css'
 import 'uno.css'
 
 const app = createApp(App)
-const pinia = createPinia()
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
 })
-pinia.use(piniaPluginPersistedstate)
+store.use(piniaPluginPersistedstate)
 app.use(router)
-app.use(pinia)
+app.use(store)
 app.use(OnuUI)
 app.mount('#app')
